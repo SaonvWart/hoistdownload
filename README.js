36d9478acbb0c0c9432c2ff13ffb2a53ea8e3fcd
SaonvWart <2055640882p@gmail.com>
2026-04-18 20:44:03 +0800

    Update download links to version 0.0.6

--- README.js
+++ README.js
@@ -7,8 +7,8 @@ const config = {
   },
   beta: {
     version: "0.0.6",
-    download: "https://github.com/SaonvWart/hoistdownload/releases/download/dev-0.0.4/hoist-dev-0.0.4.jar",
-    mod_download: "https://github.com/SaonvWart/hoistdownload/releases/download/dev-0.0.4/hoist-dev-0.0.4.jar",
+    download: "https://github.com/SaonvWart/hoistdownload/releases/download/dev-0.0.6/polaris-0.0.6.jar",
+    mod_download: "https://github.com/SaonvWart/hoistdownload/releases/download/dev-0.0.6/polaris-0.0.6.jar",
     dll_download: "none"
   }
 };
