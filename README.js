c8b5eb198918fca78433a8e43f6ee60980ffd5d8
SaonvWart <2055640882p@gmail.com>
2026-04-18 16:08:48 +0800

    Update README.js

--- README.js
+++ README.js
@@ -1,10 +1,14 @@
-export default {
+const config = {
   stable: {
     version: "none",
-    download: "none"
+    download: "none",
+    mod_download: "none",
+    dll_download: "none"
   },
   beta: {
-    version: "0.0.5",
-    download: "https://github.com/SaonvWart/hoistdownload/releases/download/dev-0.0.4/hoist-dev-0.0.4.jar"
+    version: "0.0.6",
+    download: "",
+    mod_download: "https://example.com/beta-mod.jar",
+    dll_download: "https://example.com/beta.dll"
   }
-}
+};
